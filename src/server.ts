09b38dc7b2aafa2e0f3db85765e8dd94d73b3b16
Torrecillas Pablo <22030092@itcelaya.edu.mx>
2025-06-01 09:00:06 -0600

    Agregando colors

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import Router from "./router";
 import db from "./config/db";
+import colors from "colors";
 
 //Conextar a la base de datos
 async function connectDB() {
     try {
         await db.authenticate();
         db.sync();
-        console.log("Conectado a la base de datos");
+        console.log(colors.blue.bold("Conectado a la base de datos"));
     }
     catch (error) {
         console.error(error);
-        console.log("Huevo un error al conectar a la base de datos");   
+        console.log(colors.red.bold("Huevo un error al conectar a la base de datos"));   
     }
 }
 connectDB();
