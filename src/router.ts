1db761ebad4cdaa7133534aa2c6ca9e8bdad33b6
Torrecillas Pablo <22030092@itcelaya.edu.mx>
2025-06-02 11:50:35 -0600

    Documentando ando

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,31 @@ import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
+/**  
+* @swagger
+* components:
+*       schemas:
+*               Product:
+*                       type: object
+*                       properties:    
+*                           id:
+*                                type: integer
+*                                description: The product ID
+*                                example: 1
+*                           name:
+*                                type: string
+*                                description: The product name
+*                                example: Monitor de 49 pulgadas
+*                           price:
+*                                type: number
+*                                description: The product price
+*                                example: 300
+*                           availability:
+*                                type: boolean
+*                                description: The product availability
+*                                example: true
+*/
+
 router.get("/", getProducts);
 
 router.get("/:id", 
